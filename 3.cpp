1b6c890123e8caac6665ed1f0bcb84062a75d85b
b6320502517 <sarannut.l@ku.th>
2021-03-29 18:20:22 +0700

    3

--- 3.cpp
+++ 3.cpp
@@ -1,10 +1,11 @@
 #include<stdio.h>
 int main(){
-	int n[3],i,j,k,m1,m2,m3;
+	int n[3],i,j,m1,m2,m3;
 	for(i=0;i<3;i++)
 		scanf("%d",&n[i])
 	int x[n[0]][n[0]],y[n[1]][n[1]],z[n[2][n[2]];
 	int s1[n[0]*2]={},s2[n[1]*2]={},s3[n[2]*2]={};
+	int k[3]={};
 	for(i=0;i<n[0];i++)
 		for(j=0;j<n[0];j++){
 			scanf("%d",x[i][j]);
@@ -25,7 +26,20 @@ int main(){
 			s3[i+n[2]]+=z[j][i];
 		}
 	m1=s1[0],m2=s2[0],m3=s3[0];
-	for(i=1,i<n[0])
-			
+	for(i=1,i<n[0]*2;i++)
+		if(m1==s1[i])
+			k[0]++;
+	for(i=1,i<n[1]*2;i++)
+		if(m1==s1[i])
+			k[1]++;
+	for(i=1,i<n[2]*2;i++)
+		if(m1==s1[i])
+			k[2]++;	
+	for(i=0;i<3;i++){
+		if(k[i]==n[i]*2)
+			printf("No");
+		else 
+			printf("yes");
+	}	
 	return 0;
 }
